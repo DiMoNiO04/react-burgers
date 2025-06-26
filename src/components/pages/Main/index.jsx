686169d193f1b0d433bda42ecbe0a1b094239312
Dmitry Razumov <razumov_d@ddc.by>
2025-06-26 10:58:14 +0300

    feat: add skeleton

--- src/components/pages/Main/index.jsx
+++ src/components/pages/Main/index.jsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from 'react'
 
 import { API_URL_BURGERS } from '../../../utils/consts'
-import { Card, Categories, Sort } from '../../blocks'
+import { Card, Categories, SkeletonCard, Sort } from '../../blocks'
 import { Header, Wrapper } from '../../layouts'
 import { Title } from '../../ui'
 import styles from './styles.module.scss'
 
 export const MainPageContent = () => {
   const [burgers, setBurgers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch(API_URL_BURGERS)
       .then((res) => res.json())
-      .then((data) => setBurgers(data))
+      .then((data) => {
+        setBurgers(data)
+        setIsLoading(false)
+      })
   }, [])
 
   return (
@@ -25,9 +29,7 @@ export const MainPageContent = () => {
         </div>
         <Title title={'Все пиццы'} />
         <div className={styles.cards}>
-          {burgers.map((burger) => (
-            <Card key={burger.id} {...burger} />
-          ))}
+          {isLoading ? [...new Array(8)].map((_, index) => <SkeletonCard key={index} />) : burgers.map((burger) => <Card key={burger.id} {...burger} />)}
         </div>
       </main>
     </Wrapper>
