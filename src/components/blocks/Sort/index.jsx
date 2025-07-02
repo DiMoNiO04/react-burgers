833cd219b2f4cdf5a8ad8124ae5d09cd1baeb56c
Dmitry Razumov <razumov_d@ddc.by>
2025-07-02 14:27:53 +0300

    feat: add handle click outside sort

--- src/components/blocks/Sort/index.jsx
+++ src/components/blocks/Sort/index.jsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { SORT_OPTIONS } from '../../../data'
@@ -9,6 +9,8 @@ import styles from './styles.module.scss'
 
 export const Sort = () => {
   const dispatch = useDispatch()
+
+  const sortRef = useRef(null)
 
   const [isOpenSort, setIsOpenSort] = useState(false)
 
@@ -22,8 +24,20 @@ export const Sort = () => {
     toggleOpenSort()
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sortRef.current && !event.composedPath().includes(sortRef.current)) {
+        setIsOpenSort(false)
+      }
+    }
+  
+    document.body.addEventListener('click', handleClickOutside)
+  
+    return () => document.body.removeEventListener('click', handleClickOutside)
+  }, [])
+  
   return (
-    <div className={styles.block}>
+    <div className={styles.block} ref={sortRef}>
       <button type="button" className={clsx(styles.label, isOpenSort && styles.labelOpen)} onClick={toggleOpenSort}>
         <IconArrow />
         <b>Сортировать по:</b>
